a4105fa222315c6b768251563d64de19fe466265
Bri Wyatt <mbriwyatt@gmail.com>
2023-02-04 18:58:06 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { StrictMode } from "react";
+
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <React.Fragment>
+        <StrictMode>
+            <App />
+        </StrictMode>
+    </React.Fragment>
+
+);
